cb4b451c58ea44a6ad7846ca01d262919a9706b7
AnishaDas9 <dasanisha658@gmail.com>
2024-05-26 12:04:41 +0530

    DOM

---
+++ DOM/script.js
@@ -0,0 +1,16 @@
+// let h2 = document.querySelector("h2")
+// console.dir(h2.innerText);
+// h2.innerText = h2.innerText + "from Apna College students";
+
+
+// let divs = document.querySelectorAll(".box");
+// console.log(divs[0]);
+// divs[0].innerText = "new unique value 1";
+// divs[1].innerText = "new unique value 2";
+// divs[2].innerText = "new unique value 3";
+
+
+let div = document.quarySelector("div");
+console.log(div);
+
+let id = div.getAttribute("id");
