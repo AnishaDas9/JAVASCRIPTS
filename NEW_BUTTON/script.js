ed0457b43af59a71c730529fbc59b1df7d554dc9
AnishaDas9 <dasanisha658@gmail.com>
2024-05-27 18:58:44 +0530

    NEW_BUTTON

---
+++ NEW_BUTTON/script.js
@@ -0,0 +1,13 @@
+// let newButton = document.createElement("button");
+// newBtn.innerText = "click me!";
+// console.log(newBtn);
+
+
+// let div = document.quarySelector("div");
+// div.prepend(newBtn);
+
+
+// let newHeading = document.createElement("h1");
+// newHeading.innerHTML = "<i> hi, i am new!</i>";
+
+// document.quarySelector("body").prepend(newHeading);
